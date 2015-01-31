f3a7e0d40f16e8bf3a72d4837031b1111215955a
ivandgetic <ivandgetic@gmail.com>
2015-01-31 20:23:01 +0800

    Offline messages
    Use Sqlite for offline messages

--- src/org/ivandgetic/server/app/Main.java
+++ src/org/ivandgetic/server/app/Main.java
@@ -3,13 +3,35 @@ package org.ivandgetic.server.app;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class Main {
+
     private static final int SOCKET_PORT = 50000;
     public static ArrayList<Socket> socketList = new ArrayList<Socket>();
-    public static ArrayList<String> chats=new ArrayList<String>();
+    public static int id;
+
     public static void main(String[] args) {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE MESSAGE " +
+                    "(ID INT PRIMARY KEY     NOT NULL," +
+                    " USERNAME           TEXT    NOT NULL, " +
+                    " MESSAGE            TEXT     NOT NULL) ";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -28,5 +50,5 @@ public class Main {
             }
         }).start();
     }
-
 }
+
--- src/org/ivandgetic/server/app/ServerThread.java
+++ src/org/ivandgetic/server/app/ServerThread.java
@@ -6,11 +6,17 @@ import java.io.EOFException;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class ServerThread implements Runnable {
-    private Socket socket = null;
     DataOutputStream out;
     DataInputStream in;
+    Connection c = null;
+    Statement stmt = null;
+    private Socket socket = null;
 
     public ServerThread(final Socket socket) throws IOException {
         this.socket = socket;
@@ -22,13 +28,65 @@ public class ServerThread implements Runnable {
         while (true) try {
             String readline = null;
             readline = in.readUTF();
-            String[] separate = readline.split(":", 2);
+            String[] separate = readline.split(":", 3);
             if (separate[0].equals("Message")) {
-                Main.chats.add(separate[1]);
-                System.out.println(separate[1]);
+                System.out.println(separate[1] + ":" + separate[2]);
+                try {
+                    Class.forName("org.sqlite.JDBC");
+                    c = DriverManager.getConnection("jdbc:sqlite:test.db");
+                    c.setAutoCommit(false);
+                    stmt = c.createStatement();
+                    ResultSet rs = stmt.executeQuery("SELECT * FROM MESSAGE;");
+                    while (rs.next()) {
+                        Main.id = rs.getInt("id");
+                    }
+                    rs.close();
+                    stmt.close();
+                    c.close();
+                } catch (Exception e) {
+                    System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                    System.exit(0);
+                }
+                Main.id++;
+                try {
+                    Class.forName("org.sqlite.JDBC");
+                    c = DriverManager.getConnection("jdbc:sqlite:test.db");
+                    c.setAutoCommit(false);
+                    stmt = c.createStatement();
+                    String sql = "INSERT INTO MESSAGE (ID,USERNAME,MESSAGE) " + "VALUES (" + Main.id + ", '" + separate[1] + "', '" + separate[2] + "' );";
+                    stmt.executeUpdate(sql);
+                    stmt.close();
+                    c.commit();
+                    c.close();
+                    Main.id++;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
                 for (Socket socket : Main.socketList) {
                     out = new DataOutputStream(socket.getOutputStream());
-                    out.writeUTF(separate[1]);
+                    out.writeUTF("Message:" + separate[1] + ":" + separate[2]);
+                }
+            } else if (separate[0].equals("Operate")) {
+                if (separate[1].equals("GetAllMessage")) {
+                    try {
+                        Class.forName("org.sqlite.JDBC");
+                        c = DriverManager.getConnection("jdbc:sqlite:test.db");
+                        c.setAutoCommit(false);
+                        stmt = c.createStatement();
+                        ResultSet rs = stmt.executeQuery("SELECT * FROM MESSAGE;");
+                        while (rs.next()) {
+                            String userName = rs.getString("USERNAME");
+                            String message = rs.getString("MESSAGE");
+                            out = new DataOutputStream(socket.getOutputStream());
+                            out.writeUTF("Operate:" + userName + ":" + message);
+                        }
+                        rs.close();
+                        stmt.close();
+                        c.close();
+                    } catch (Exception e) {
+                        System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                        System.exit(0);
+                    }
                 }
             }
         } catch (SocketException e) {
@@ -43,5 +101,4 @@ public class ServerThread implements Runnable {
             e.printStackTrace();
         }
     }
-
 }
